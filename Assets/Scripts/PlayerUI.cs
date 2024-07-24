9f378d9917f35b45feb835c722c1d2482075ba28
calvinmckbeav <92180625+calvinmckbeav@users.noreply.github.com>
2024-07-24 13:40:26 -0400

    full hades level design

---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    public Text itemInfo;
+    public Text dialogueText;
+    public Slider healthSlider;
+    public Image reticle;
+
+
+    private bool inDialogue;
+
+    private Talkable talker;
+
+
+    void Start()
+    {
+        itemInfo.gameObject.SetActive(false);
+        dialogueText.gameObject.SetActive(false);
+
+        inDialogue = false;
+    }
+
+    private void Update()
+    {
+        if (!inDialogue)
+        {
+            TextDetect();
+        }
+        else
+        {
+            DialogueSequence();
+        }
+    }
+
+    void TextDetect()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.gameObject.TryGetComponent(out ItemAttribute item))
+            {
+                itemInfo.text = item.textDescription;
+                itemInfo.gameObject.SetActive(true);
+
+                if (Input.GetKeyDown(item.interactKey))
+                {
+                    item.Interact();
+                }
+            }
+
+            /*else if (hit.collider.gameObject.TryGetComponent(out TalkableAttribute source))
+            {
+                itemInfo.text = source.textDescription;
+                itemInfo.gameObject.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.P))
+                {
+                    DialogueON(source);
+                }
+            }
+            */
+
+            else
+            {
+                itemInfo.gameObject.SetActive(false);
+            }
+        }
+
+    }
+
+    public void DialogueON(Talkable talker)
+    {
+        this.talker = talker;
+
+        MouseLook.active = false;
+        PlayerController.active = false;
+
+        inDialogue = true;
+        dialogueText.text = talker.GetDialogue();
+
+        itemInfo.gameObject.SetActive(false);
+        dialogueText.gameObject.SetActive(true);
+        healthSlider.gameObject.SetActive(false);
+        reticle.gameObject.SetActive(false);
+    }
+
+    public void DialogueOFF()
+    {
+        MouseLook.active = true;
+        PlayerController.active = true;
+
+        inDialogue = false;
+
+        dialogueText.gameObject.SetActive(false);
+        healthSlider.gameObject.SetActive(true);
+        reticle.gameObject.SetActive(true);
+    }
+
+    void DialogueSequence()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            dialogueText.text = talker.GetDialogue();
+        }
+    }
+
+
+}
